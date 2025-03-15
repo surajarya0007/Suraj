d792fb582c2c71063b7c38beda812729dd736fd3
surajarya0007 <aryasuraj@gmail.com>
2025-03-15 17:31:28 +0530

    vercel test 4 font 2

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
